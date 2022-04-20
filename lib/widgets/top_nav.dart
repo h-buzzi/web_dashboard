7a04d2b180e1bb72fefa819b4d5bb9ddab4139f7
h-buzzi <h-buzzi>
2022-04-20 16:44:43 -0300

    top navigation

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -3,6 +3,24 @@ import 'package:web_dashboard/helpers/responsiveness.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                )
+              ],
+            )
+          : IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                key.currentState!.openDrawer();
+              },
+            ),
       elevation: 0,
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
     );
